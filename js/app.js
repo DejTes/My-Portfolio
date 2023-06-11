6fbe358d8025b50d2fe193f3ab52dfb21823e4ba
dejtes <dejenaref@gmail.com>
2023-06-11 18:57:06 -0400

    skills section modified

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const navbar = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
@@ -26,16 +25,14 @@ closeBtn.addEventListener("click", function () {
 });
 
 // close sidebar when a sidebar link is clicked
-sidebarLinks.forEach(link => {
+sidebarLinks.forEach((link) => {
   link.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar");
   });
 });
 
-
 // set year
 date.innerHTML = new Date().getFullYear();
-
 
 //toggles the navigation menu in contact me page
 document.addEventListener("DOMContentLoaded", function () {
@@ -48,56 +45,77 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // contact me form function
-function sendEmail(){
+function sendEmail() {
   Email.send({
-      SecureToken: "65955C083D8FBF4D085D947943B6C0796790",
-      To : 'dejenaref@gmail.com',
-      From : document.getElementById("email").value,
-      Subject : "New Contact",
-      Body : "Name: " + document.getElementById('name').value
-            + "<br> Email: " + document.getElementById('email').value
-            + "<br> Phone Num: " + document.getElementById('phone').value
-            + "<br> Message: " + document.getElementById('message').value
-  }).then(
-message => alert('Message Sent successfully!')
-);
+    SecureToken: "65955C083D8FBF4D085D947943B6C0796790",
+    To: "dejenaref@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New Contact",
+    Body:
+      "Name: " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Phone Num: " +
+      document.getElementById("phone").value +
+      "<br> Message: " +
+      document.getElementById("message").value,
+  }).then((message) => alert("Message Sent successfully!"));
 }
 
 //ScrollReveal
-document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelectorAll('nav a');
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("nav a");
 
-  navLinks.forEach(link => {
-      link.addEventListener('click', (e) => {
-          e.preventDefault();
-          const target = e.target.getAttribute('href');
-          const targetElement = document.querySelector(target);
-          targetElement.scrollIntoView({ behavior: 'smooth' });
-      });
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const target = e.target.getAttribute("href");
+      const targetElement = document.querySelector(target);
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    });
   });
 
-  
   // Initialize ScrollReveal
   const sr = ScrollReveal({
-      distance: '50px',
-      duration: 2000,
-      easing: 'ease-in-out',
-      reset: true,
+    distance: "50px",
+    duration: 2000,
+    easing: "ease-in-out",
+    reset: true,
   });
 
   // Reveal animation for projects section
-  sr.reveal('#projects', { origin: 'left' });
-  sr.reveal('#home', { origin: 'top' });
+  sr.reveal("#projects", { origin: "left" });
+  sr.reveal("#home", { origin: "top" });
 
   // Reveal animation for about me section
-  sr.reveal('#about', { origin: 'right' });
+  sr.reveal("#about", { origin: "right" });
 
-  sr.reveal('#contact', { origin: 'bottom' });
-  sr.reveal('#footer', { origin: 'right' });
- 
- 
+  sr.reveal("#contact", { origin: "bottom" });
+  sr.reveal("#footer", { origin: "right" });
 });
 
+// skills section
 
+function fadeInSkills() {
+  const skillsBoxes = document.querySelectorAll(".skills-box");
 
+  let delay = 0;
+  skillsBoxes.forEach((box) => {
+    setTimeout(() => {
+      box.style.opacity = 1;
+      box.style.transform = "translateX(0)";
+    }, delay);
+    delay += 150; // Adjust the delay time between each skill box
+  });
+}
 
+window.addEventListener("scroll", () => {
+  const skillsSection = document.querySelector(".skills-section");
+  const skillsSectionTop = skillsSection.offsetTop;
+  const windowHeight = window.innerHeight;
+
+  if (window.pageYOffset > skillsSectionTop - windowHeight + 200) {
+    fadeInSkills();
+  }
+});
